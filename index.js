445af8a54c87fd30d7a762e5f111467f9a5461d6
Stephen <stephenkago10@gmail.com>
2022-08-19 01:05:09 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: " ",
+    streetAddress: " "
+  };
+  //"before each" hook for "returns an employee with the original key value pairs and the new key value pair"
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let myEmployee = { ...employee, [key]: value };
+    return myEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    let myEmployee = { ...employee };
+    delete myEmployee[key];
+    return myEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
